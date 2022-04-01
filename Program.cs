401cc2ded545459944ffb1db52f08710855948b3
Rutte Silva <rosrosrutte@gmail.com>
2022-04-01 20:54:36 -0300

    Finaliza o exercício

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int numeroDigitado;
+
+Console.Write("Digite um número: ");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+bool numeroEhPar = (numeroDigitado % 2) == 0;
+
+if (numeroEhPar)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine($"{numeroDigitado} é par");
+}
+
+else 
+{
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    Console.WriteLine($"{numeroDigitado} é impar");
+}
+
+Console.ResetColor();
